aae8cfaa331dc9810efd77d39e761fbabc8e27ce
AndryDubov <xeonmao@gmail.com>
2017-05-31 09:09:53 -0700

    initial project version

---
+++ src/by/stromnet/calcu/Application.java
@@ -0,0 +1,11 @@
+package by.stromnet.calcu;
+
+/**
+ * Created by andry on 5/26/17.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
